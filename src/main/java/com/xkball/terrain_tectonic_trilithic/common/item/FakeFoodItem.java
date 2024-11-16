695c18e5e3938689d8c3542fa74f2fc54ac66711
xkball <45330674+xkball@users.noreply.github.com>
2024-11-17 02:01:47 +0800

    fix bug : cannot use equals to compare item stacks
    Took 4 hours 4 minutes

--- src/main/java/com/xkball/terrain_tectonic_trilithic/common/item/FakeFoodItem.java
+++ src/main/java/com/xkball/terrain_tectonic_trilithic/common/item/FakeFoodItem.java
@@ -5,6 +5,8 @@ import com.xkball.terrain_tectonic_trilithic.api.annotation.NonNullByDefault;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
+import java.util.Objects;
+
 @NonNullByDefault
 public class FakeFoodItem extends Item {
     
@@ -14,6 +16,19 @@ public class FakeFoodItem extends Item {
     
     public record FakeItem(ItemStack itemStack){
         public static final Codec<FakeItem> CODEC = ItemStack.CODEC.xmap(FakeItem::new,FakeItem::itemStack);
+        
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            FakeItem fakeItem = (FakeItem) o;
+            return ItemStack.isSameItemSameComponents(itemStack,fakeItem.itemStack);
+        }
+        
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(itemStack);
+        }
     }
     
     
